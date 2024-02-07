532d692d54840ffa5edeee6d90e5c65e3e929909
Viktor Kudryavtsev <viktork8888@gmail.com>
2024-02-07 21:16:59 +0200

    added exercises_waist files

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 'use strict';
 
 import('./js/exercises.js');
+import('./js/exercises_waist.js');
 import('./js/exercises_modal.js');
+import('./js/favorites.js');
 import('./js/footer.js');
 import('./js/quote.js');
+import('./js/mobile_menu.js');
+import('./js/rating_modal.js');
