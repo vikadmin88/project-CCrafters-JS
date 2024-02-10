06979d948466258cbc55899ed06f80d7ff134596
vikadmin88 <viktork8888@gmail.com>
2024-02-10 15:53:44 +0000

    Deploying to gh-pages from @ vikadmin88/project-CCrafters-JS@406b9775b3c5d29f0b65208273e3300d53deb544 🚀

---
+++ assets/footer-399ffbd4.js
@@ -0,0 +1,2 @@
+import{i as e}from"./vendor-7f21c288.js";e.settings({position:"topRight",timeout:5e3,resetOnHover:!0,transitionIn:"flipInX",transitionOut:"flipOutX"});function s(o,t){switch(o){case"error":e.error({message:t});break;case"warning":e.warning({message:t});break;case"success":e.success({message:t});break}}const i=Object.freeze(Object.defineProperty({__proto__:null},Symbol.toStringTag,{value:"Module"})),n=Object.freeze(Object.defineProperty({__proto__:null},Symbol.toStringTag,{value:"Module"}));export{i as e,n as f,s as n};
+//# sourceMappingURL=footer-399ffbd4.js.map
