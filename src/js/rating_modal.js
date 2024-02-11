7956720293fca3ec1fe97a60aa689097479fc74c
iryshanykkolaevna <141450700+iryshanykkolaevna@users.noreply.github.com>
2024-02-11 12:11:32 +0200

    modal rating

--- src/js/rating_modal.js
+++ src/js/rating_modal.js
@@ -1,123 +1,81 @@
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-const modal = document.getElementById('myModal');
-const closeBtn = document.querySelector('.close');
-const sendBtn = document.getElementById('sendBtn');
-const emailInput = document.getElementById('email');
-const commentInput = document.getElementById('comment');
-const form= document.querySelectorAll('.modal-content');
+import { notify } from './notifier.js';
+import { API_EXERCISES_POINT, api } from './api.js';
 
-form.addEventListener('submit', submitForm);
+const refs = {
+  icons: document.querySelectorAll('.rating-star-icon'),
+  form: document.querySelector('.rating-form'),
+  stars: document.querySelector('.rating-star-list'),
+};
 
-function submitForm(e) {
-  e.preventDefault;
-}
-
-apiReqParams = {
-  "rate": 4,
-  "email": "test@gmail6.com",
-  "review": "My best exercise"
-}
-api.patch(API_EXERCISES_POINT + "/64f389465ae26083f39b17a2/rating", apiReqParams)
-  .then(response => {
-    console.log(response);
-    notify("success", "Rating has been updated!");
-  })
-  .catch(error => {
-    console.log(error);
-    if (error.message.response.status === 409) {
-      notify("warning", `Warning: ${error.message.response.data.message}`);
-    } else {
-      notify("error", `API error: ${error.message.response.data.message}`);
+refs.icons.forEach((icon, index) => {
+  icon.addEventListener('mouseover', () => {
+    for (let i = 0; i < index; i++) {
+      refs.icons[i].classList.add('hovered');
     }
   });
 
+  icon.addEventListener('mouseout', () => {
+    for (let i = 0; i < index; i++) {
+      refs.icons[i].classList.remove('hovered');
+    }
+  });
+});
 
+refs.form.addEventListener('submit', sendRating);
+refs.stars.addEventListener('click', changeRating);
 
+function changeRating(event) {
+  if (event.target.nodeName !== 'INPUT') {
+    return;
+  }
 
-// sendBtn.addEventListener('click', function() {
-//   const email = emailInput.value;
-//   const comment = commentInput.value;
-//   const rating = getRating();
+  const rating = event.currentTarget.previousElementSibling;
 
-//   if (isEmailUsed(email)) {
-//     alert('Ця адреса електронної пошти вже використовується!');
-//     return;
-//   }
+  rating.textContent = parseFloat(event.target.value).toFixed(1);
 
-//   sendDataToServer(email, comment, rating);
-// });
+  const stars = Array.from(
+    event.currentTarget.querySelectorAll('.rating-star-icon')
+  );
+  const index = stars.indexOf(event.target.nextElementSibling);
 
+  stars.forEach((star, i) => {
+    star.classList.toggle('selected', i <= index);
+  });
+}
 
-// stars.forEach(star => {
-//   star.addEventListener('click', function() {
-//     this.classList.toggle('selected');
-//   });
-// });
+function sendRating(event) {
+  event.preventDefault();
 
+  const rate = parseInt(refs.form.querySelector('.rating-value').textContent);
+  const email = event.currentTarget.elements.email.value;
+  const review = event.currentTarget.elements.comment.value;
 
-// function getRating() {
-//   const selectedStars = document.querySelectorAll('.star.selected');
-//   return selectedStars.length;
-// }
+  const ratingParams = {
+    rate,
+    email,
+    review,
+  };
 
+  console.log(ratingParams);
+  console.log(JSON.stringify(ratingParams));
 
-// function sendDataToServer(email, comment, rating) {
-//   fetch('url_', {
-//     method: 'POST',
-//     headers: {
-//       'Content-Type': 'application/json',
-//     },
-//     body: JSON.stringify({
-//       email: email,
-//       comment: comment,
-//       rating: rating,
-//     }),
-//   })
-//   .then(response => {
-//     if (!response.ok) {
-//       throw new Error('Network response was not ok');
-//     }
-//     return response.json();
-//   })
-//   .then(data => {
-//     handleServerResponse(data);
-//   })
-//   .catch(error => {
-//     console.error('There was a problem with your fetch operation:', error);
-//   });
-// }
+  const id = '64f389465ae26083f39b182b';
 
-
-// function handleServerResponse(data) {
-// }
-
-
-// function isEmailUsed(email) {
-//   return usedEmails.includes(email);
-// }
-
-
-// function showModal() {
-//   modal.style.display = 'block';
-// }
-
-// function closeModal() {
-//   modal.style.display = 'none';
-// }
-// document.querySelectorAll('.star').forEach(item => {
-//   item.addEventListener('click', event => {
-//     const stars = Array.from(item.parentNode.children);
-//     const index = stars.indexOf(item);
-    
-//     // Встановлюємо активний клас для всіх зірок, які менше або дорівнюють поточній
-//     stars.forEach((star, i) => {
-//       star.classList.toggle('active', i <= index);
-//     });
-
-//     // Оновлюємо значення рейтингу
-//     const ratingValue = document.querySelector('.rating-value');
-//     ratingValue.textContent = (index + 1); 
-//   });
-// });
+  api
+    .patch(`${API_EXERCISES_POINT}/${id}/rating`, JSON.stringify(ratingParams))
+    .then(response => {
+      console.log(response);
+      notify('success', 'Rating has been updated!');
+    })
+    .catch(error => {
+      console.log(error);
+      if (error.message.response.status === 409) {
+        notify('warning', `Warning: ${error.message.response.data.message}`);
+      } else {
+        notify('error', `API error: ${error.message.response.data.message}`);
+      }
+    });
+}
