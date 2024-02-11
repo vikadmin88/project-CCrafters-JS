b2430f360dff896c7711a768e0b96345d90a3491
KirillShytov <sitovk587@gmail.com>
2024-02-11 23:56:24 +0200

    update

--- src/js/mobile_menu.js
+++ src/js/mobile_menu.js
@@ -1,6 +1,10 @@
-const openModal = document.querySelector('.backdrop');
+const modal = document.querySelector('.backdrop');
 const closeModal = document.querySelector('.btn-close');
+const link = document.querySelector('.btn-link-section');
 
 closeModal.addEventListener('click', () => {
-  openModal.classList.remove('is-open');
+  modal.classList.remove('is-open');
+});
+link.addEventListener('click', () => {
+  modal.classList.remove('is-open');
 });
