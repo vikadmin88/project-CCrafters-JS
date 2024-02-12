edbea93b4c2b349c35d2c8e591617d7da3a5fca3
KirillShytov <sitovk587@gmail.com>
2024-02-12 15:27:00 +0200

    update button

--- src/js/mobile_menu.js
+++ src/js/mobile_menu.js
@@ -5,9 +5,9 @@ const favoritesButton = document.querySelector('.link-favorites');
 
 const pathname = window.location.pathname;
 
-if (pathname === '/' || pathname === '/index.html') {
+if (pathname.endsWith('/') || pathname.endsWith('/index.html')) {
   homeButton.classList.add('active');
-} else if (pathname === '/favorites.html') {
+} else if (pathname.endsWith('/favorites.html')) {
   favoritesButton.classList.add('active');
 } else {
   console.log('Невідома сторінка:', pathname);
