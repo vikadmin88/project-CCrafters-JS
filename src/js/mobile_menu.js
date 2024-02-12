8ff3120afc2b4d1dd69cd278bddbf38c41d834c1
KirillShytov <sitovk587@gmail.com>
2024-02-12 14:07:36 +0200

    update

--- src/js/mobile_menu.js
+++ src/js/mobile_menu.js
@@ -1,10 +1,18 @@
 const modal = document.querySelector('.backdrop');
-const closeModal = document.querySelector('.btn-close');
-const link = document.querySelector('.btn-link-section');
+const closeModal = document.querySelector('.btn-close-mob');
+const homeButton = document.querySelector('.first-link-home');
+const favoritesButton = document.querySelector('.link-favorites');
+
+const pathname = window.location.pathname;
+
+if (pathname === '/' || pathname === '/index.html') {
+  homeButton.classList.add('active');
+} else if (pathname === '/favorites.html') {
+  favoritesButton.classList.add('active');
+} else {
+  console.log('Невідома сторінка:', pathname);
+}
 
 closeModal.addEventListener('click', () => {
-  modal.classList.remove('is-open');
-});
-link.addEventListener('click', () => {
   modal.classList.remove('is-open');
 });
